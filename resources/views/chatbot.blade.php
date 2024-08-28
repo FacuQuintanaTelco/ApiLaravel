a0d34f803fa023ff0b45ccb60c1f1f2cd4c3e7e4
Facundo Quintana <facundo.quintana@telco.com.ar>
2024-08-28 12:58:18 -0300

    Corregido Scroll

--- resources/views/chatbot.blade.php
+++ resources/views/chatbot.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('/assets/css/jquery.mCustomScrollbar.min.css') }}">
     <script src="{{ asset('/assets/js/jquery-2.1.3.min.js') }}"></script>
     <script src="{{ asset('/assets/js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
 
